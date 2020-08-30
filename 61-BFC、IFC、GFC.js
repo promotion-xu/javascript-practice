b5bdd5b2de3fcb32a76c87da82e219c3bf7815a3
xuzhen <17521337708@163.com>
2020-08-30 14:59:34 +0800

    feat: 61

---
+++ 61-BFC、IFC、GFC.js
@@ -0,0 +1,9 @@
+// 1. BFC: 页面上的一个隔离的渲染区域，那么他是如何产生的呢？
+// 可以触发BFC的元素有float、position、overflow、display：table-cell/ inline-block/table-caption ；
+// BFC有什么作用呢？比如说实现多栏布局’
+
+// 2. IFC:
+
+// 3. GFC:
+
+// 4. FFC
