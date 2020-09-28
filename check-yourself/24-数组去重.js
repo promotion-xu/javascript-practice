210f3e9e81b59cfb03e014e64e7ec1f5750a40fd
promotion-xu <17521337708@163.com>
2020-09-28 17:33:01 +0800

    feat: 24

---
+++ check-yourself/24-数组去重.js
@@ -0,0 +1,23 @@
+let arr = [1, 1, 1, 2, 2, 2, 3, 4, 4];
+
+// 1. Set
+// console.log([...new Set(arr)]);
+
+// 2. includes
+// let newArr = [];
+// arr.forEach((v, i) => {
+//   if (newArr.includes(v)) {
+//   } else {
+//     newArr.push(v);
+//   }
+// });
+// console.log(newArr);
+
+// 3. indexOf
+// let newArr = [];
+// for (let i = 0, length = arr.length; i < length; i++) {
+//   if (newArr.indexOf(arr[i]) < 0) {
+//     newArr.push(arr[i]);
+//   }
+// }
+// console.log(newArr);
