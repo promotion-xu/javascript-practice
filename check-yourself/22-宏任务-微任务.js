538c3fbe71bb4ee9b50fc8fd4f9a0022464e9e3b
promotion-xu <17521337708@163.com>
2020-09-28 16:50:12 +0800

    feat: 22

---
+++ check-yourself/22-宏任务-微任务.js
@@ -0,0 +1,3 @@
+// 宏任务： setTimeout，setInterval，setImmediate (Node独有)，requestAnimationFrame (浏览器独有)，I/O，UI rendering (浏览器独有)
+
+// 微任务： process.nextTick (Node独有)，Promise，Object.observe，MutationObserver
