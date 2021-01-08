77aa437405196d1cbfd1c474725640e7b8b9a441
promotion-xu <17521337708@163.com>
2021-01-08 15:36:45 +0800

    feat: 26

---
+++ check-yourself/26-fib.js
@@ -0,0 +1,13 @@
+// const fib = (n) => {
+//   if (n <= 2) return 1;
+//   return fib(n - 1) + fib(n - 2);
+// };
+
+// console.log(fib(45));
+
+// const fib = (n, prev = 1, next = 1) => {
+//   if (n <= 1) return prev;
+//   return fib(n - 1, next, next + prev);
+// };
+
+// console.log(fib(40));
