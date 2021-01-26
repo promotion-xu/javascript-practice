4cf84d4e0bf7eddb60e89338c37d71c80fe1ed87
promotion-xu <17521337708@163.com>
2021-01-26 14:36:24 +0800

    feat: 02

--- javascript-1/06-广度-深度优先遍历.js
+++ javascript-1/06-广度-深度优先遍历.js
@@ -13,13 +13,13 @@ const tree = [
   },
 ];
 
-// 1. 深度优先遍历
+// 深度遍历
 const depth = (tree) => {
   let nodes = [];
   fla(tree);
-  function fla(arr) {
-    arr.forEach((v) => {
-      nodes.push(v.name);
+  function fla(tree) {
+    tree.forEach((v) => {
+      nodes.push({ name: v.name });
       if (v.children) {
         fla(v.children);
       }
@@ -28,28 +28,63 @@ const depth = (tree) => {
   return nodes;
 };
 
-const arr = depth(tree);
-console.log("arr", arr);
+// 广度优先遍历
 
-// 2. 广度优先遍历
-const breadth = (node) => {
+const breadth = (tree) => {
   let nodes = [];
-  let stack = [];
-  if (node) {
-    stack.push(node);
-    while (stack.length) {
-      let item = stack.shift();
-      let children = item.children;
-      nodes.push(item);
-      // 队列，先进先出
-      // nodes = [] stack = [parent]
-      // nodes = [parent] stack = [child1,child2,child3]
-      // nodes = [parent, child1] stack = [child2,child3,child1-1,child1-2]
-      // nodes = [parent,child1,child2]
-      for (let i = 0; i < children.length; i++) {
-        stack.push(children[i]);
-      }
+  let stacks = [];
+  stacks = [...tree];
+  while (stacks.length) {
+    console.log("stacks", stacks);
+    let item = stacks.shift();
+    let children = item?.children;
+    nodes.push({ name: item.name });
+    for (let i = 0; i < children.length; i++) {
+      stacks.push(children[i]);
     }
   }
+
   return nodes;
 };
+console.log("nodes", breadth(tree));
+
+// 1. 深度优先遍历
+// const depth = (tree) => {
+//   let nodes = [];
+//   fla(tree);
+//   function fla(arr) {
+//     arr.forEach((v) => {
+//       nodes.push(v.name);
+//       if (v.children) {
+//         fla(v.children);
+//       }
+//     });
+//   }
+//   return nodes;
+// };
+
+// const arr = depth(tree);
+// console.log("arr", arr);
+
+// // 2. 广度优先遍历
+// const breadth = (node) => {
+//   let nodes = [];
+//   let stack = [];
+//   if (node) {
+//     stack.push(node);
+//     while (stack.length) {
+//       let item = stack.shift();
+//       let children = item.children;
+//       nodes.push(item);
+//       // 队列，先进先出
+//       // nodes = [] stack = [parent]
+//       // nodes = [parent] stack = [child1,child2,child3]
+//       // nodes = [parent, child1] stack = [child2,child3,child1-1,child1-2]
+//       // nodes = [parent,child1,child2]
+//       for (let i = 0; i < children.length; i++) {
+//         stack.push(children[i]);
+//       }
+//     }
+//   }
+//   return nodes;
+// };
