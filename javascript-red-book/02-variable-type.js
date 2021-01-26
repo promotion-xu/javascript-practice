54be0255daa4079a409146383e52ccfe2203a608
promotion-xu <17521337708@163.com>
2021-01-26 16:27:52 +0800

    feat: 02

--- javascript-red-book/02-variable-type.js
+++ javascript-red-book/02-variable-type.js
@@ -3,13 +3,16 @@
 
 // 当然，如果使用 instanceof 操作符检测基本类型的
 // 值，则该操作符始终会返回 false，因为基本类型不是对象
-console.log(1 instanceof Object); // false
+// console.log(1 instanceof Object); // false
 
-console.log("1" instanceof Object); /// false
+// console.log("1" instanceof Object); /// false
 
 // 所有引用类型的值都是 Object 的实例。因此，在检测一个引用类型值和 Object 构造
 // 函数时，instanceof 操作符始终会返回 true。
 
-console.log(Array instanceof Object); // true
-console.log(Function instanceof Object); //true
-console.log(RegExp instanceof Object); // true
+// console.log(Array instanceof Object); // true
+// console.log(Function instanceof Object); //true
+// console.log(RegExp instanceof Object); // true
+
+console.log([] instanceof Array);
+console.log(Array.isArray([]));
