d92d0c53c3d4a4641bb09d55768bfb5fa143342c
promotion-xu <17521337708@163.com>
2021-02-02 10:17:53 +0800

    feat: 13

--- 面试题/13-abcd 变为badc.js
+++ 面试题/13-abcd 变为badc.js
@@ -1,3 +1,25 @@
-let str = "abcd";
+let str = "abcdef";
 
-// => 'badc'
+//字符串两两交换 => 'badcfe'
+
+function exchange(str) {
+  if (str.length % 2 !== 0) return str;
+  let odd = [];
+  let even = [];
+  let result = "";
+  for (let i = 0, len = str.length; i < len; i++) {
+    if (i % 2 === 0) {
+      // 偶数位
+      even.push(str[i]);
+    } else {
+      // 奇数位
+      odd.push(str[i]);
+    }
+  }
+  odd.forEach((v, i) => {
+    result += v + even[i];
+  });
+  return result;
+}
+
+console.log(exchange(str));
