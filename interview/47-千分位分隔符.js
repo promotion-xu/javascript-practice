a131bcf5b1102a7474ab81fd432860e6e5d96a0f
promotion-xu <17521337708@163.com>
2021-02-25 17:45:57 +0800

    feat: 47

--- interview/47-千分位分隔符.js
+++ interview/47-千分位分隔符.js
@@ -17,5 +17,29 @@ function numFormat(num) {
   return res;
 }
 
-console.log(numFormat(123456789.234)); // 123,456,789.234
-console.log(numFormat(23456789.234)); // 123,456,789.234
+// console.log(numFormat(123456789.234)); // 123,456,789.234
+// console.log(numFormat(23456789.234)); // 123,456,789.234
+
+function trim(str) {
+  return str.replace(/^\s+/, "").replace(/\s+$/, "");
+}
+
+// console.log(trim(" s af fdsf  "));
+
+function deepClone(obj) {
+  if (obj === null) return null;
+  if (typeof obj !== "object") return obj;
+  const result = Array.isArray(obj) ? [] : {};
+  for (key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      result[key] =
+        typeof obj[key] === "object" ? deepClone(obj[key]) : obj[key];
+    }
+  }
+  return result;
+}
+
+// console.log(deepClone(null));
+// console.log(deepClone({ name: "xz" }));
+// console.log(deepClone(1));
+// console.log(deepClone(function a() {}));
