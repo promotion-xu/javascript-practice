0f8c2655b1edf92ae20632df8eee7bc85c67db7c
promotion-xu <17521337708@163.com>
2021-03-10 22:55:10 +0800

    feat: init cpromise

---
+++ promise/index.js
@@ -0,0 +1,21 @@
+/**
+ * 1. promise是一个类,new Promise
+ * 2. new Promise第一个参数是一个构造器，立即被执行
+ * 3. promise 状态一旦被改变，就不可再次更改
+ * 4. then 方法接受成功，失败的函数
+ */
+
+const myPromise = require("./promise");
+
+let p = new myPromise((resolve, reject) => {
+  resolve("成功");
+});
+
+p.then(
+  value => {
+    console.log(value);
+  },
+  reason => {
+    console.log(reason);
+  }
+);
---
+++ promise/promise.js
@@ -0,0 +1,40 @@
+const PENDING = "pending";
+const FULFILLED = "fulfilled";
+const REJECTED = "rejected";
+
+class myPromise {
+  constructor(executor) {
+    executor(this.resolve, this.reject);
+  }
+
+  status = PENDING;
+  // 成功后的值
+  value = undefined;
+  // 失败后的原因
+  reason = undefined;
+
+  // 箭头函数内没有this指向，所以外面调用resolve('')
+  resolve = value => {
+    // 状态一旦被改变，就不可再次更改
+    if (this.status !== PENDING) return;
+    this.status = FULFILLED;
+    this.value = value;
+  };
+
+  reject = reason => {
+    // 状态一旦被改变，就不可再次更改
+    if (this.status !== PENDING) return;
+    this.status = REJECTED;
+    this.reason = reason;
+  };
+
+  then(successCallback, failCallback) {
+    if (this.status === FULFILLED) {
+      successCallback(this.value);
+    } else if (this.status === REJECTED) {
+      failCallback(this.reason);
+    }
+  }
+}
+
+module.exports = myPromise;
