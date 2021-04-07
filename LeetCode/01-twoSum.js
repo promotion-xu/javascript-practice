d3832e03464ff16c64b8a165437135aacb301236
promotion-xu <17521337708@163.com>
2021-04-07 14:10:50 +0800

    feat: 01

--- LeetCode/01-twoSum.js
+++ LeetCode/01-twoSum.js
@@ -1,14 +1,26 @@
+// function twoSum(nums, target) {
+//   let length = nums.length;
+//   for (let i = 0; i < length; i++) {
+//     for (let j = i + 1; j < length; j++) {
+//       if (nums[i] + nums[j] === target) {
+//         return [i, j];
+//       }
+//     }
+//   }
+//   return [];
+// }
+
 function twoSum(nums, target) {
-  let length = nums.length;
-  for (let i = 0; i < length; i++) {
-    for (let j = i + 1; j < length; j++) {
-      if (nums[i] + nums[j] === target) {
-        return [i, j];
-      }
-    }
+  let length = nums.length
+  const MAP = new Map()
+  MAP.set(nums[0], 0)
+  for (let i = 1; i < length; i ++) {
+    let other = target - nums[i]
+    if (MAP.get(other) !== undefined) return [MAP.get(other), i]
+    MAP.set(nums[i], i)
   }
-  return [];
 }
+
 
 let arr = [1, 2, 3, 4, 5];
 
