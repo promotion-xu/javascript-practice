a7ee8fc14c371e13068665081d4b9c77d41e82b4
promotion-xu <17521337708@163.com>
2022-04-03 17:12:23 +0800

    feat: encode

--- core/js/06-数字进行编码压缩.js
+++ core/js/06-数字进行编码压缩.js
@@ -1,2 +1,39 @@
-Input: 'aaaabbbccd'
-Output: 'a4b3c2d1'，代表 a 连续出现四次，b连续出现三次，c连续出现两次，d连续出现一次
+// Input: 'aaaabbbccd'
+// Output: 'a4b3c2d1'
+
+const str = "aaaabbbccd"
+const dict = {
+  a: 4,
+  b: 3,
+  c: 2,
+  d: 1
+}
+// function encode(str) {
+//   const dict = {}
+//   for (const s of str) {
+//     dict[s] = (dict[s] || 0) + 1
+//   }
+//   let result = ''
+//   Object.keys(dict).forEach(item => {
+//     result += item + dict[item]
+//   })
+//   return result
+// }
+// console.log(encode(str)) // a4b3c2d1
+
+// 如果只出现一次， 则 a4b3c2d
+
+function encode(str) {
+  const dict = {}
+  for (const s of str) {
+    dict[s] = (dict[s] || 0) + 1
+  }
+  let result = ''
+  Object.keys(dict).forEach(item => {
+    if (dict[item] === 1) dict[item] = ''
+    result += item + dict[item]
+  })
+  return result 
+}
+
+console.log(encode(str))
