2369afdae57aa952148eac3c7af4ce48584593b3
promotion-xu <17521337708@163.com>
2022-04-04 10:19:54 +0800

    feat: shuffle

--- core/lodash/06-shuffle.js
+++ core/lodash/06-shuffle.js
@@ -1,4 +1,12 @@
-题目: 【Q447】如何实现一个数组洗牌函数 shuffle
-相关: 【Q645】随机生成六位数的手机验证码(重复/不可重复)
-代码: 【Q447】如何实现一个数组洗牌函数 shuffle
-对于实现一个简单的 shuffle，可能极其简单。
+// 题目: 【Q447】如何实现一个数组洗牌函数 shuffle
+// 相关: 【Q645】随机生成六位数的手机验证码(重复/不可重复)
+// 代码: 【Q447】如何实现一个数组洗牌函数 shuffle
+// 对于实现一个简单的 shuffle，可能极其简单。
+
+
+const arr = [3, 1, 5, 4]
+
+//随机洗牌函数
+const shuffle = (list) => list.sort((x, y) => Math.random() - 0.5)
+
+console.log(shuffle(arr))
