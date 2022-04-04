396a95d147cdac9019372b5162dcb0c8785f56f7
promotion-xu <17521337708@163.com>
2022-04-04 10:15:50 +0800

    feat: compose

---
+++ core/lodash/05-compose.js
@@ -0,0 +1,27 @@
+
+
+// function compose(...fns) {
+//   return fns.reduce((f, g) => {
+//     return (...args) => {
+//       return f(g(...args))
+//     }
+//   })
+// }
+
+// 从左向右计算
+const composeLeft = (...fns) => fns.reduce((f,g) => (...args) => g(f(...args)))
+
+// 从右向左计算
+const composeRight = (...fns) => fns.reduceRight((f, g) => (...args) => g(f(...args)))
+const add5 = x => x + 5
+const multiply = x => x * 10
+const left = composeLeft(
+  add5,
+  multiply
+)
+const right = composeRight(
+  add5,
+  multiply
+)
+console.log(left(10)) // 105
+console.log(right(10)) // 105
